09d8853933b33b7120749e5e7886531f60077122
nikidav9 <nikidav9@gmail.com>
2026-09-19 10:46:17 +0300

    Remove Telegram from worker consent

--- app/register-worker.tsx
+++ app/register-worker.tsx
@@ -303,7 +303,7 @@ export default function RegisterWorker() {
                     >
                       трансграничную передачу персональных данных
                     </Text>
-                    {' '}для push/web-push уведомлений и подключаемого Telegram. Отказ не мешает регистрации.
+                    {' '}для push/web-push уведомлений. Отказ не мешает регистрации.
                   </Text>
                 </View>
               </TouchableOpacity>
