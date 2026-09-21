88fce2fa0e627872eaabec5c5981351d596bb3ad
nikidav9 <nikidav9@gmail.com>
2026-09-21 11:29:10 +0300

    Extend PWA viewport into iOS status bar

--- app/+html.tsx
+++ app/+html.tsx
@@ -7,7 +7,7 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
         <title>JobToo</title>
         <meta name="description" content="Работа в Москве — свайпайте и откликайтесь" />
